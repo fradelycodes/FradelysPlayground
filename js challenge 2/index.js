4a3ae720e69ee12a0987c06de52f88ec437e3204
fradelycode <fradelydventura@gmail.com>
2021-05-10 20:20:08 -0400

    add object

---
+++ js challenge 2/index.js
@@ -0,0 +1,37 @@
+function setUp() {
+    // Create a person object that contains three keys: name, age, and county.
+    let person = {
+        name: "Fradely",
+        age: "26",
+        country: "DOM",
+    };
+
+    // Create a function, logData(), that uses the person object to create a
+    function logData() { 
+    alert(`${person.name}`)
+    }
+    
+logData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+window.onload = function () {
+  setUp();
+};
